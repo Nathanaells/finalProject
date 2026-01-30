f7ab065a0ec2bfd3d38cac76e04fcbc34635f357
Nathanaells <nathanaelsinaga27@gmail.com>
2026-01-31 02:45:13 +0700

    adding error handler

--- server/middlewares/errorHandler.js
+++ server/middlewares/errorHandler.js
@@ -1,8 +1,40 @@
 const errorHandler = (err, req, res, next) => {
-  console.error(err.stack);
-  res
-    .status(500)
-    .json({ message: "Internal Server Error", error: err.message });
+  let status = 500;
+  let message = "Internal Server Error";
+
+  if (
+    err.name === "SequelizeUniqueConstraintError" ||
+    err.name === "SequelizeValidationError"
+  ) {
+    status = 400;
+    message = err.errors.map((e) => e.message);
+  }
+
+  if (err.name === "JsonWebTokenError") {
+    status = 401;
+    message = "Invalid Token";
+  }
+
+  if (err.name === "ValidationError") {
+    status = 400;
+    message = err.message;
+  }
+
+  if (err.name === "UnauthorizedError") {
+    status = 401;
+    message = "Unauthorized";
+  }
+
+  if (err.name === "ForbiddenError") {
+    status = 403;
+    message = "Forbidden";
+  }
+  if (err.name === "NotFoundError") {
+    status = 404;
+    message = "Not Found";
+  }
+
+  res.status(status).json({ message, error: err.message });
 };
 
 module.exports = errorHandler;
